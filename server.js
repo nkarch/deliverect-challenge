8118ad83d3732b467169d0d315238c265e280595
Nick Karch <nickkarch@Nicks-MacBook-Pro.local>
2023-11-17 17:49:51 -0500

    Add CORS middleware to server

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const POINT_OF_SALES = require('./pos.json');
-const CHANNELS = require('./channel.json');
+const fs = require("fs");
+const POINT_OF_SALES = require("./pos.json");
+const CHANNELS = require("./channel.json");
+const cors = require("cors");
 
 const PORT = 5000;
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/pos', (req, res) => {
+app.get("/pos", (req, res) => {
     res.json(POINT_OF_SALES);
 });
 
-app.get('/channel', (req, res) => {
+app.get("/channel", (req, res) => {
     res.json(CHANNELS);
 });
 
-app.post('/account', (req, res) => {
+app.post("/account", (req, res) => {
     const accountPayload = req.body;
     const filename = `account_${Date.now()}.json`;
 
     if (!accountPayload || Object.keys(accountPayload).length === 0) {
-        res.status(400).send({"error": "Missing payload"});
+        res.status(400).send({ error: "Missing payload" });
     }
 
     console.log("Saving data to disk:", accountPayload);
